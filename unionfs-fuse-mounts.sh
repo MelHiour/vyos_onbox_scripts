33ec566120f1bbfa8a8259302f198deaf90401ee
Mikhail Ermakov <melhiour@gmail.com>
2021-07-13 17:16:48 +0100

    Create unionfs-fuse-mounts.sh

---
+++ unionfs-fuse-mounts.sh
@@ -0,0 +1,10 @@
+#!/bin/vbash
+source /opt/vyatta/etc/functions/script-template
+
+if [ "$(id -g -n)" != 'vyattacfg' ] ; then
+    exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
+fi
+
+_LENTH=$(ps aux | grep unionfs-fuse | wc -l)
+logger "Number of unionfs-fuse mount points is $_LENTH"
+exit
