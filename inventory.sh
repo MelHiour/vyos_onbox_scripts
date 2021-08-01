37f64f7632c363f076e2ab7724b696d1b31d2642
Mikhail Ermakov <melhiour@gmail.com>
2021-08-01 16:58:16 +0100

    Update inventory.sh

--- inventory.sh
+++ inventory.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+_INVENTORY_PATH=https://whale:3000/inventory
 
 if [ "$1" == "get" ]; then
-    curl -kn --silent https://whale:3000/inventory$2 | python -m json.tool
+    curl -kn --silent $_INVENTORY_PATH$2 | python -m json.tool
 elif [ "$1" == "post" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request POST --data $2 https://whale:3000/inventory$3 | python -m json.tool
+    curl -kn --silent --header "Content-Type:application/json" --request POST --data $2 $_INVENTORY_PATH$3 | python -m json.tool            
 elif [ "$1" == "put" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request PUT --data $2 https://whale:3000/inventory$3 | python -m json.tool
+    curl -kn --silent --header "Content-Type:application/json" --request PUT --data $2 $_INVENTORY_PATH$3 | python -m json.tool            
 elif [ "$1" == "delete" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request DELETE https://whale:3000/inventory$2 | python -m json.tool
+    curl -kn --silent --header "Content-Type:application/json" --request DELETE $_INVENTORY_PATH$2 | python -m json.tool            
 fi
