8db389909248cd72b35f97a5adf20e683f8bd3da
Konstantin Demin <rockdrilla@gmail.com>
2021-08-03 12:24:28 +0300

    inventory: rewrite
    make whitespaces consistent across file
    also trim trailing ones

--- inventory.sh
+++ inventory.sh
@@ -3,14 +3,14 @@
 # This is a wrapper to query inventory API described in https://github.com/MelHiour/simple_api_inventory
 # To make it work you need to
 # - Have .netrc in your home directory
-#    $ cat .netrc 
+#    $ cat .netrc
 #    machine whale
-#	    login melhiour
-#	    password dfsjhsjdhfjd87erk
+#        login melhiour
+#        password dfsjhsjdhfjd87erk
 # - Store this script in /config/scripts/init.sh (if you use VyOS)
-# - Fix permissions 
+# - Fix permissions
 #    chmod +x /config/scripts/init.sh
-# - Make a simlink 
+# - Make a simlink
 #    sudo ln -s /config/scripts/inv.sh /usr/local/bin/inv
 #
 # Enjoy!
@@ -32,14 +32,37 @@
 
 _INVENTORY_PATH=https://whale:3000/inventory
 
-if [ "$1" == "get" ]; then
-    curl -kn --silent $_INVENTORY_PATH$2 | python -m json.tool
-elif [ "$1" == "post" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request POST --data "$2" $_INVENTORY_PATH$3 | python -m json.tool            
-elif [ "$1" == "put" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request PUT --data "$2" $_INVENTORY_PATH$3 | python -m json.tool            
-elif [ "$1" == "delete" ]; then
-    curl -kn --silent --header "Content-Type:application/json" --request DELETE $_INVENTORY_PATH$2 | python -m json.tool    
-else
-    echo 'Operation is not supported. Please use [get|put|delete|post]'
-fi
+set -f
+
+need_args() {
+    [ "$2" = "$3" ] && return
+    echo "Action '$1' requires $2 arguments - got $3 instead" 1>&2
+    exit 1
+}
+
+do_req() {
+    curl -sSLkn "$@" | python -m json.tool
+}
+
+case "$1" in
+get)
+    need_args "$1" 2 $#
+    do_req "${_INVENTORY_PATH}$2"
+;;
+post)
+    need_args "$1" 3 $#
+    do_req "${_INVENTORY_PATH}$3" -X POST -H "Content-Type: application/json" --data "$2"
+;;
+put)
+    need_args "$1" 3 $#
+    do_req "${_INVENTORY_PATH}$3" -X PUT -H "Content-Type: application/json" --data "$2"
+;;
+delete)
+    need_args "$1" 2 $#
+    do_req "${_INVENTORY_PATH}$2" -X DELETE -H "Content-Type: application/json"
+;;
+*)
+    echo 'Operation is not supported. Please use [get|put|delete|post]' 1>&2
+    exit 1
+;;
+esac
